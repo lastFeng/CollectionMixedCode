5e83725c153d05bbc011672504881ba8da90b7d9
lastFeng <563805728@qq.com>
2019-08-14 13:39:25 +0800

    rabbitmq

---
+++ rabbitma-hello/src/main/java/com/learn/rabbitmahello/RabbitmaHelloApplication.java
@@ -0,0 +1,13 @@
+package com.learn.rabbitmahello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmaHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmaHelloApplication.class, args);
+    }
+
+}
